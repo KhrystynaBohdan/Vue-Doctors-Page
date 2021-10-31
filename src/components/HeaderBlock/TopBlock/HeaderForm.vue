360a2a5d6e10e4c8e95214013e1a9ac27182b1d6
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-10-31 22:40:10 +0200

    added submit (console)

--- src/components/HeaderBlock/TopBlock/HeaderForm.vue
+++ src/components/HeaderBlock/TopBlock/HeaderForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="top-block__appointment">
+  <form class="top-block__appointment" @submit.prevent="handleSubmit">
     <div class="top-block__form-title">Book Appointment</div>
     <label>Name *</label>
     <input
@@ -7,26 +7,27 @@
       autocomplete="on"
       class="top-block__input"
       placeholder="Full Name"
+      v-model="name" required
     />
     <label>Email *</label>
     <input
-      type="text"
+      type="email"
       autocomplete="on"
       class="top-block__input"
       placeholder="example@gmail.com"
+      v-model="email" required
     />
     <label>Department *</label>
-    <select class="top-block__select" tabindex="0">
+    <select class="top-block__select" tabindex="0" v-model="department" required>
       <option value="select">Please Select</option>
       <option value="surgery">Surgery</option>
       <option value="pediatric">Pediatric</option>
     </select>
     <label>Time *</label>
-    <select class="top-block__select" tabindex="0">
-      <option value="time">4:00 Available</option>
-      <option value="time">5:00 Available</option>
-      <option value="time">6:00 Available</option>
-      ]
+    <select class="top-block__select" tabindex="0" v-model="time" required>
+      <option value="time1">4:00 Available</option>
+      <option value="time2">5:00 Available</option>
+      <option value="time3">6:00 Available</option>
     </select>
     <button class="top-block__submit button-primary">Book Appointment</button>
   </form>
@@ -34,8 +35,28 @@
 
 <script>
 export default {
-  name: "HeaderForm",
-};
+  data() {
+    return {
+      email: "",
+      name: '',
+      department: 'surgery',
+      time: 'time1'
+    };
+  },
+  methods: {
+    handleSubmit(){
+      console.log('name: ', this.name)
+      console.log('email: ', this.email)
+      console.log('department: ', this.department)
+      console.log('time: ', this.time)
+
+      this.name = ''
+      this.email = ''
+      this.department = ''
+      this.time = ''
+    }
+  }
+  }
 </script>
 
 <style scoped lang="scss">
@@ -100,6 +121,13 @@ export default {
   &__submit {
     margin-top: 40px;
     font-size: 14px;
+  }
+
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
   }
 }
 </style>
